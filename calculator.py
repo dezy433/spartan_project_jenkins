782530fa8cb7508cbc75c36bf2186f64388c75c1
derenertim@hotmail.com <derenertim@hotmail.com>
2022-04-06 16:34:56 +0100

    adding calculator test using py-test

---
+++ calculator.py
@@ -0,0 +1,12 @@
+def test_add():
+    assert add(0,1)== 1
+    assert add(0,1)== 1
+    assert add(0,1)== 1
+    assert add(0,1)== 1
+    assert add(0,1)== 1
+    assert add(0,1)== 2
+    assert add(0,1)== 1
+    assert add(0,1)== 1
+
+def add(number1,number2):
+    return number1 + number2
